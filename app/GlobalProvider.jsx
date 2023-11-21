762ee96a89cfa9414e13daecbd684e4243c4a4de
skdksldk <tnwns1485@naver.com>
2023-11-21 18:26:47 +0900

    Add Register and Login Functionality

--- app/GlobalProvider.jsx
+++ app/GlobalProvider.jsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/CartContext";
+import { ToastContainer } from "react-toastify";
+import { SessionProvider } from "next-auth/react";
 
 export function GlobalProvider({ children }) {
-  return <CartProvider>{children}</CartProvider>;
+  return (
+    <>
+      <ToastContainer position="bottom-right" />
+      <AuthProvider>
+        <CartProvider>
+          <SessionProvider>{children}</SessionProvider>
+        </CartProvider>
+      </AuthProvider>
+    </>
+  );
 }
